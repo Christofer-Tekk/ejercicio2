75d2c519f1a2e1c8035ea1b844076e0fae9ddffe
cristian6160 <cristianburper@gmail.com>
2025-09-28 21:26:23 -0400

    Add: funcion logaritmo

---
+++ src/AritmeticaLogaritmo.java
@@ -0,0 +1,6 @@
+public class AritmeticaLogaritmo{
+    public static double logaritmo(int valorNum, int baseLog){
+    double resLogaritmo = Math.log(valorNum) / Math.log(baseLog);
+    return resLogaritmo;
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("2+3 = " + AritmeticaSumar.sumar(2,3));
     System.out.println("27-13 = " + AritmeticaRestar.restar(27,13));
+    System.out.println("log 8 base 2 = " + AritmeticaLogaritmo.logaritmo(8,2));
     // Agregan llamadas de las otras clases
   }
 }
