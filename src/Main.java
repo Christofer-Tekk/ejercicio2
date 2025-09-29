b66bbdc4c491f4566a9afd104f58a70a1c079259
Angela Tarque <angela.tarque26@gmail.com>
2025-09-28 23:56:53 -0400

    add: raiz cuadrada

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     System.out.println("2+3 = " + AritmeticaSumar.sumar(2,3));
     System.out.println("27-13 = " + AritmeticaRestar.restar(27,13));
     System.out.println("log 8 base 2 = " + AritmeticaLogaritmo.logaritmo(8,2));
+    System.out.println("la raiz cuadrada de 81 = " + AritmeticalRaizCuadrada.Raiz(81));
     // Agregan llamadas de las otras clases
   }
 }
