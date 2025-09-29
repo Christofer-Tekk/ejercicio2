552ed6dac7bd9689e811571c88102fbe8c3a70b9
David <jorgeescalera09@gmail.com>
2025-09-29 04:46:34 -0400

    realiza una division

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     System.out.println("27-13 = " + AritmeticaRestar.restar(27,13));
     System.out.println("log 8 base 2 = " + AritmeticaLogaritmo.logaritmo(8,2));
     System.out.println("la raiz cuadrada de 81 = " + AritmeticalRaizCuadrada.Raiz(81));
+    System.out.println("28/3 =" + AritmeticaDividir.dividir(28,3));
     // Agregan llamadas de las otras clases
   }
 }
